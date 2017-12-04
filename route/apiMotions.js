371840904c4e0fa8fe832105e95ee7702631dc4e
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-04 05:51:48 +0300

    add create motion function

--- route/apiMotions.js
+++ route/apiMotions.js
@@ -8,3 +8,16 @@ const db = require('../models')(Sequelize, config);
 const app = express();
 app.use(bodyParser.json()); 
 module.exports = router;
+
+router.post('/create',async function(req,res,next){
+    let result = await db.motions.create(req.body);
+    
+        if (result !== undefined)
+        {
+            res.send(JSON.stringify(result));
+        }
+        else
+        {
+            res.end('ERROR CREATE');
+        }
+});
